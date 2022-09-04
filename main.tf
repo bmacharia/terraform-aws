1a4a2957dcdb49ccd6620c3e6fd081b7f3f79325
Babu Macharia <babu.macharia@gmail.com>
2022-09-03 21:10:17 -0700

    Resource block missing closing bracket

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_internet_gateway" "main" {
 
 }
 
-resource "aws_eip" "nat"
+resource "aws_eip" "nat" {
                                                                    
   count = length(var.public_cidr)                                                                     
 
