5803fccd88c0edc04ff8ab4b74bd1ad1048afacf
Babu Macharia <babu.macharia@gmail.com>
2022-09-20 11:55:24 -0700

    Add ec2 instances

--- provider.tf
+++ provider.tf
@@ -1,6 +1,3 @@
-
-# the provider is the bridge between terraform and the cloud provider
-# terraform provider interfaces with cloud vendor api's t build infrastucture
 terraform {
   required_providers {
     aws = {
@@ -9,9 +6,10 @@ terraform {
     }
   }
 }
-# provider block supplies the info needed to access aws
+
 provider "aws" {
   region                   = "us-west-2"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "kenna"
 }
+
