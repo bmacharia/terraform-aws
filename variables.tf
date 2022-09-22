77f9e147d52acde46769dd1062a99e8535165b3a
Babu Macharia <babu.macharia@gmail.com>
2022-09-22 12:39:55 -0700

    double check that there is newline at EOF

--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "env_code" {
   type = string
 }
 
-variable vpc_cidr {}
+variable "vpc_cidr" {}
 
-variable  private_cidr {}
+variable "private_cidr" {}
 
-variable public_cidr {}
+variable "public_cidr" {}
 
-variable my_public_ip {}
+variable "my_public_ip" {}
