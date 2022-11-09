10916cfb4b4b2521fdb4e270505b6f6417d10850
Babu Macharia <babu.macharia@gmail.com>
2022-11-09 12:35:06 -0800

    Add level1

---
+++ level1/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+    
+    backend "s3" {
+    bucket = "terraform-remote-state-babuki"
+    key    = "level1.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "terraform-remote-state"
+  }
+
+}
+
+provider "aws" {
+  region                   = "us-west-2"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "kenna"
+}
---
+++ level1/variables.tf
@@ -0,0 +1,11 @@
+variable "env_code" {
+  type = string
+}
+
+variable "vpc_cidr" {}
+
+variable "private_cidr" {}
+
+variable "public_cidr" {}
+
+variable "my_public_ip" {}
