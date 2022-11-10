5227cc03d20c54061d92a3801e6a0fe31653fa58
Babu Macharia <babu.macharia@gmail.com>
2022-11-10 10:11:59 -0800

    fix formatting

--- level2/provider.tf
+++ level2/provider.tf
@@ -5,14 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
-    
-    backend "s3" {
-    bucket = "terraform-remote-state-babuki"
-    key    = "level2.tfstate"
-    region = "us-west-2"
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-babuki"
+    key            = "level2.tfstate"
+    region         = "us-west-2"
     dynamodb_table = "terraform-remote-state"
   }
-
 }
 
 provider "aws" {
