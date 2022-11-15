71f16a3092de3cef6094cbeb0f15299bd6bdacac
Babu Macharia <babu.macharia@gmail.com>
2022-11-15 10:54:07 -0800

    update dyanmodb table name

--- level0/main.tf
+++ level0/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "terraform_remote_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_remote_state" {
-  name           = "terraform-remote-state-"
+  name           = "terraform-remote-state"
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
