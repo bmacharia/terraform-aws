6bdb18a79e20815aa972ec6794e1a1a5771ed03e
Babu Macharia <babu.macharia@gmail.com>
2022-12-17 10:22:56 -0800

    IAM

--- level2/asg.tf
+++ level2/asg.tf
@@ -1,10 +1,11 @@
 resource "aws_launch_configuration" "main" {
-  name_prefix     = "${var.env_code}-"
-  image_id        = data.aws_ami.amazonlinux.id
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.private.id]
-  user_data       = file("user-data.sh")
-  key_name        = "main"
+  name_prefix          = "${var.env_code}-"
+  image_id             = data.aws_ami.amazonlinux.id
+  instance_type        = "t3.micro"
+  security_groups      = [aws_security_group.private.id]
+  user_data            = file("user-data.sh")
+  key_name             = "main"
+  iam_instance_profile = aws_iam_instance_profile.main.name
 
 }
 
