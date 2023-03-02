88af36ff662824c1ae9cd1e33cd45403a30605ff
Babu Macharia <babu.macharia@gmail.com>
2023-03-02 09:25:16 -0800

    change region to us-east-1

--- level2/data.tf
+++ level2/data.tf
@@ -3,6 +3,6 @@ data "terraform_remote_state" "level1" {
   config = {
     bucket = "terraform-remote-state-babuki"
     key    = "level1.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
   }
 }
