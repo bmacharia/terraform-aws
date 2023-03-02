35a64c33a9c4ea63ec732760da780ef3432ddffa
Babu Macharia <babu.macharia@gmail.com>
2023-03-01 21:48:28 -0800

    Change remote backend region

--- level0/provider.tf
+++ level0/provider.tf
@@ -5,10 +5,11 @@ terraform {
       version = "~> 4.0"
     }
   }
+
 }
 
 provider "aws" {
-  region                   = "us-west-2"
+  region                   = "us-east-1"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "terraform-user"
 }
--- level1/provider.tf
+++ level1/provider.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "aws" {
-  region                   = "us-west-2"
+  region                   = "us-east-1"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "terraform-user"
 }
